f944f4b2dc7e668883338da97b0d330bf5900901
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2021-12-26 18:16:05 +0700

    Create menuKaryawan.java

---
+++ ProgramKaryawan/src/programkaryawan/menuKaryawan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programkaryawan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public abstract class menuKaryawan {
+    
+   abstract public void tambah(DataKaryawan dataK);
+   abstract public void hapus(String kode);
+   abstract public void cari(String kode);
+   
+}
